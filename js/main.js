272f4f03c3b4e22d033906d81b3eb483aff04ce0
Julien Kempf <44439875+ChaKkaLLV@users.noreply.github.com>
2021-03-07 14:39:17 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ const rightBox = document.querySelector('.right-box')
 white.addEventListener('click', () => {
     white.classList.add('link-active');
     dark.classList.remove('link-active');
-    leftBox.classList.remove('dark-theme')
-    rightBox.classList.remove('dark-theme')
+    leftBox.classList.toggle('dark-theme')
+    rightBox.classList.toggle('dark-theme')
 });
 dark.addEventListener('click', () => {
     dark.classList.add('link-active');
     white.classList.remove('link-active');
-    leftBox.classList.add('dark-theme')
-    rightBox.classList.add('dark-theme')
+    leftBox.classList.toggle('dark-theme')
+    rightBox.classList.toggle('dark-theme')
 });
 
